7d5a992def556581fb489f411cb3c5cbf57f7061
Jon Harper <jon.harper87@gmail.com>
2017-06-29 14:32:45 +0200

    add comments, disable some things

--- main_datathon.py
+++ main_datathon.py
@@ -9,9 +9,11 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 
+#IMPORTATION DES DONNEES
 df_origin= pd.read_csv('consommation-electrique-par-secteurs-dactivite.csv', delimiter=';')
 df_origin2= pd.read_csv('production-electrique-par-filiere.csv', delimiter=';')
 
+#INDEXATION PAR LE CODE IRIS
 tags = ['Code IRIS','Nb sites secteur résidentiel',
        'Conso totale secteur résidentiel (MWh)',
        'Conso moyenne secteur résidentiel (MWh)', 'Nb sites Agriculture',
@@ -21,46 +23,49 @@ tags = ['Code IRIS','Nb sites secteur résidentiel',
 df_origin_group=df_origin.groupby('Code IRIS')
 df_origin_group1=df_origin_group.get_group('100150000')
 
+#LIMITATION A LA DERNIERE ANNEE
 idx_last_yr = df_origin.groupby(['Code IRIS'], sort=False)['Année'].transform(max) == df_origin['Année']
 df_origin_last_yr=df_origin[idx_last_yr]
 df_origin_last_yr_trunc=df_origin_last_yr[tags]
 
 df_origin_last_yr_trunc=df_origin_last_yr_trunc.set_index('Code IRIS')
+
+#NORMALISATION A [0,1]
 df=df_origin_last_yr_trunc
 df -= df.min()
 df /= df.max()
 
+#REMPLISSAGE DES CASES VIDES PAR DES 0
 df_woNan=df.fillna(0)
 
 
-from pandas.plotting import scatter_matrix
-
+#from pandas.plotting import scatter_matrix
 #scatter_matrix(df, alpha=0.2, figsize=(6, 6), diagonal='kde')
 #df_origin_last_yr_trunc_minmax = df_origin_last_yr_trunc.transform(df_origin_last_yr_trunc)
+
+#REDUCTION DE DIMENSION POUR VISUALISATION
 from sklearn.manifold import TSNE
-
-nbrnanrow=df_woNan.isnull().sum(axis=1)
-nbrnanrcol=df_woNan.isnull().sum(axis=0)
 model = TSNE(n_components=2, random_state=0)
-
 np.set_printoptions(suppress=True) 
 
+#LIMITATION DU NOMBRE DE SAMPLES
 nb_samples=1000
 import random
 random_idx=random.sample(range(len(df_woNan.index)), nb_samples)
-
 ndarray_tsne=model.fit_transform(df_woNan.ix[random_idx]) 
-df_tsne=pd.DataFrame(ndarray_tsne,columns=['a', 'b'])
 
+#PLOT EN 2D PRE-CLUSTER
+df_tsne=pd.DataFrame(ndarray_tsne,columns=['a', 'b'])
 df_tsne.plot.scatter(x='a', y='b');
 
+#CLUSTERISATION HIERARCHIQUE
 from sklearn.cluster import AgglomerativeClustering
 hierarch_cluster=AgglomerativeClustering(n_clusters=8)
 cluster_nmb= np.zeros(shape=(nb_samples,1))
 cluster_nmb=hierarch_cluster.fit_predict(df_tsne, y=cluster_nmb)
 
+#PLOT EN 2D POST-CLUSTER
 from matplotlib import pyplot as plt
-# Visualize the clustering
 def plot_clustering(X_red, y,labels, title=None):
     x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
     X_red = (X_red - x_min) / (x_max - x_min)
@@ -78,10 +83,15 @@ def plot_clustering(X_red, y,labels, title=None):
     plt.show()
 plot_clustering(df_tsne.as_matrix(), hierarch_cluster.labels_, hierarch_cluster.labels_, "Ward linkage")
 
+#PLOT EN 2D POST-CLUSTER ALTERNATIF (SANS NUMERO DE CLUSTER)
 #from matplotlib import pyplot
 #pyplot.scatter(df_tsne.as_matrix()[:,0], df_tsne.as_matrix()[:,1], c=hierarch_cluster.labels_)
 #pyplot.show()
+
+#AJOUT DU NUMERO DU CLUSTER
 df_woNan_selec=df_woNan.ix[random_idx]
 df_woNan_selec['cluster']=hierarch_cluster.labels_
+
+#STATISTIQUES PAR CLUSTER
 df_woNan_selec_group=df_woNan_selec.groupby('cluster')
 df_woNan_selec_stats=df_woNan_selec_group.mean()
