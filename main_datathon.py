45651f012f96a361f768fe3f1b63b17773b060f8
Jon Harper <jon.harper87@gmail.com>
2017-06-30 13:23:15 +0200

    v5

--- main_datathon.py
+++ main_datathon.py
@@ -11,7 +11,7 @@ from sklearn import preprocessing
 
 #IMPORTATION DES DONNEES
 df_origin= pd.read_csv('all_data_v1_1_infos_iris.csv', delimiter=',')
-
+df_origin_position= df_origin[['lon','lat']]
 #INDEXATION PAR LE CODE IRIS
 tags = ['id_iris','c_elec_conso_tot_res',
        'c_elec_conso_tot_agr', 'c_elec_conso_tot_indus',
@@ -24,22 +24,13 @@ tags2 = ['c_elec_conso_tot_res',
        'c_elec_conso_tot_agr', 'c_elec_conso_tot_indus',
        'c_elec_conso_tot_tert', 'p_elec_prod_pv',
        'p_elec_prod_eol','p_elec_prod_bio','p_elec_prod_coge']
-       
+tags3=[s+'_par_hbts' for s in tags2]
 test=df_origin[tags2].values/df_origin[['socioeco_npers']].values
-df_origin[tags2]=pd.DataFrame(test)
+df_origin[tags3]=pd.DataFrame(test)
 
-#test=df_origin['socioeco_npers'].values/df_origin[['aire_iris']].values
-#df_origin['socioeco_npers']=pd.DataFrame(test)
-
-df_origin=df_origin[tags]
-
-#df_origin=df_origin.set_index(['id_iris'])
-
-#NORMALISATION A [0,1]
-#df=df_origin
-#df -= df.min()
-#df /= df.max()
-
+tags3.append('id_iris')
+df_origin=df_origin[tags3]
+df_origin=df_origin.set_index(['id_iris'])
 #REMPLISSAGE DES CASES VIDES PAR DES 0
 df_woNan=df_origin.fillna(0)
 
@@ -51,51 +42,47 @@ np.set_printoptions(suppress=True)
 import time
 start_time = time.time()
 #LIMITATION DU NOMBRE DE SAMPLES
-nb_samples=len(df_woNan.index)
+nb_samples=1000#len(df_woNan.index)
+nb_clusters=10
 
 import random
-#random_idx=random.sample(range(len(df_woNan.index)), nb_samples)
-random_idx=np.array(range(len(df_woNan.index)))
+random_idx=random.sample(range(len(df_woNan.index)), nb_samples)
+#random_idx=np.array(range(len(df_woNan.index)))
 
 #CLUSTERISATION HIERARCHIQUE
 from sklearn.cluster import AgglomerativeClustering
-hierarch_cluster=AgglomerativeClustering(n_clusters=10)
+hierarch_cluster=AgglomerativeClustering(n_clusters=nb_clusters)
 cluster_nmb= np.zeros(shape=(nb_samples,1))
-cluster_nmb=hierarch_cluster.fit_predict(df_woNan.ix[random_idx], y=cluster_nmb)
+cluster_nmb=hierarch_cluster.fit_predict(df_woNan.iloc[random_idx], y=cluster_nmb)
 print("--- %s seconds ---" % (time.time() - start_time))
 #
-#ndarray_tsne=model.fit_transform(df_woNan.ix[random_idx]) 
+ndarray_tsne=model.fit_transform(df_woNan.iloc[random_idx]) 
 ##PLOT EN 2D PRE-CLUSTER
-#df_tsne=pd.DataFrame(ndarray_tsne,columns=['a', 'b'])
-#df_tsne.plot.scatter(x='a', y='b');
+df_tsne=pd.DataFrame(ndarray_tsne,columns=['a', 'b'])
+df_tsne.plot.scatter(x='a', y='b');
 #
 ##PLOT EN 2D POST-CLUSTER
-#from matplotlib import pyplot as plt
-#def plot_clustering(X_red, y,labels, title=None):
-#    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
-#    X_red = (X_red - x_min) / (x_max - x_min)
-#    plt.figure(figsize=(6, 4))
-#    for i in range(X_red.shape[0]):
-#        plt.text(X_red[i, 0], X_red[i, 1], str(y[i]),
-#                 color=plt.cm.spectral(labels[i] / 10.),
-#                 fontdict={'weight': 'bold', 'size': 9})
-#    plt.xticks([])
-#    plt.yticks([])
-#    if title is not None:
-#        plt.title(title, size=17)
-#    plt.axis('on')
-#    plt.show()
-#plot_clustering(df_tsne.as_matrix(), hierarch_cluster.labels_, hierarch_cluster.labels_, "Ward linkage")
-#
-###PLOT EN 2D POST-CLUSTER ALTERNATIF (SANS NUMERO DE CLUSTER)
-#from matplotlib import pyplot
-#pyplot.scatter(df_tsne.as_matrix()[:,0], df_tsne.as_matrix()[:,1], c=hierarch_cluster.labels_)
-#pyplot.show()
+from matplotlib import pyplot as plt
+def plot_clustering(X_red, y,labels, title=None):
+    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
+    X_red = (X_red - x_min) / (x_max - x_min)
+    plt.figure(figsize=(6, 4))
+    for i in range(X_red.shape[0]):
+        plt.text(X_red[i, 0], X_red[i, 1], str(y[i]),
+                 color=plt.cm.spectral(labels[i] / 10.),
+                 fontdict={'weight': 'bold', 'size': 9})
+    plt.xticks([])
+    plt.yticks([])
+    if title is not None:
+        plt.title(title, size=17)
+    plt.axis('on')
+    plt.show()
+plot_clustering(df_tsne.as_matrix(), hierarch_cluster.labels_, hierarch_cluster.labels_, "Clusters énergétiques en France")
 
-from matplotlib import pyplot
-pyplot.figure()
-pyplot.scatter(df_origin2[['lon']].as_matrix(), df_origin2[['lat']].as_matrix(), c=hierarch_cluster.labels_)
-pyplot.show()
+df_origin_position_small=df_origin_position.ix[random_idx]
+plot_clustering(df_origin_position_small.as_matrix(), cluster_nmb,cluster_nmb, title=None)
+#pyplot.scatter(df_origin_position[['lon']].ix[random_idx].as_matrix(), df_origin_position[['lat']].ix[random_idx].as_matrix(), c=hierarch_cluster.labels_)
+#pyplot.show()
 
 #AJOUT DU NUMERO DU CLUSTER
 df_woNan_selec=df_woNan.ix[random_idx]
@@ -105,26 +92,14 @@ df_woNan_selec['cluster']=hierarch_cluster.labels_
 df_woNan_selec_group=df_woNan_selec.groupby('cluster')
 df_woNan_selec_stats=df_woNan_selec_group.mean()
 
-#from sklearn.neighbors import NearestNeighbors
-#nbrs = NearestNeighbors(n_neighbors=4, algorithm='ball_tree').fit(df_woNan_selec[['x','y']].dropna())
-#distances, indices = nbrs.kneighbors(df_woNan_selec[['x','y']])
+from sklearn.neighbors import NearestNeighbors
+nbrs = NearestNeighbors(n_neighbors=4, algorithm='ball_tree').fit(df_woNan)
+distances, indices = nbrs.kneighbors(df_woNan)
 
 
 #indicestest=pd.DataFrame(indices).loc[:1000].applymap(lambda x:df_positionIRIS.ix[x,['INSEE COMM']].valu)
-#from sklearn.svm import OneClassSVM
-#outlier_detect_svc=OneClassSVM()
-#outlier_detect_svc.fit(df_tsne)
-#outliers_fraction = 0.25
-#scores_pred = outlier_detect_svc.decision_function(df_tsne)
-#y_pred = outlier_detect_svc.predict(df_tsne)
-#
-##PLOT EN 2D POST-CLUSTER ALTERNATIF (SANS NUMERO DE CLUSTER)
-#from matplotlib import pyplot
-#plt.figure()
-#
-#pyplot.scatter(df_tsne.as_matrix()[:,0], df_tsne.as_matrix()[:,1], c=y_pred)
-#pyplot.show()
 
-output=df_woNan[['id_iris']]
-output['Classe']=cluster_nmb
-output.to_csv('IRIS_10classes_ecoseulmnt.csv', sep=';', index=False)
+
+#output=df_woNan[['id_iris']]
+#output['Classe']=cluster_nmb
+#output.to_csv('IRIS_10classes_ecoseulmnt.csv', sep=';', index=False)
